1fb03d6e975d38f907bd102917f1aba08046fdb1
Kristin Skipper <kskipper.03@gmail.com>
2020-05-11 15:20:49 -0500

    initial

---
+++ src/main/java/com/Kristin/springsecurityldap/HomeResource.java
@@ -0,0 +1,14 @@
+package com.Kristin.springsecurityldap;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HomeResource {
+	
+	@GetMapping("/")
+	public String index() {
+		return ("<h1>Home Page</h1>");
+	}
+
+}
